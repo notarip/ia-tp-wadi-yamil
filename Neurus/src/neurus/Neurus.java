7f20306957511d75f7393ec7d91875bc25728029
wadijm@gmail.com <wadijm@gmail.com@16f6e4bd-8b96-7e31-a378-faec98637aed>
2011-11-27 22:15:43 +0000

    No anda un carajo

--- Neurus/src/neurus/Neurus.java
+++ Neurus/src/neurus/Neurus.java
@@ -4,95 +4,154 @@
  */
 package neurus;
 
-import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
-import javax.imageio.ImageIO;
+import java.util.Observable;
+import java.util.Observer;
 import org.neuroph.contrib.imgrec.FractionRgbData;
-import org.neuroph.contrib.imgrec.ImageRecognitionSample;
-import org.neuroph.contrib.imgrec.ImageSampler;
 import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.learning.SupervisedLearning;
 import org.neuroph.core.learning.SupervisedTrainingElement;
 import org.neuroph.core.learning.TrainingElement;
 import org.neuroph.core.learning.TrainingSet;
-import org.neuroph.nnet.Hopfield;
 import org.neuroph.nnet.MultiLayerPerceptron;
-import org.neuroph.nnet.Perceptron;
 import org.neuroph.nnet.learning.LMS;
 import org.neuroph.nnet.learning.MomentumBackpropagation;
-import org.neuroph.util.TransferFunctionType;
 
 /**
  *
  * @author ringo
  */
-public class Neurus {
+public class Neurus implements Observer{
 
     /**
      * @param args the command line arguments
+     * 
      */
-    public static void main(String[] args) throws IOException, Exception {
-        
-
-        int maxIterations = 800;
-        NeuralNetwork neuralNet = new MultiLayerPerceptron(TransferFunctionType.TANH, 2,4,1);
-        ((LMS) neuralNet.getLearningRule()).setMaxError(0.01);//0-1
+    
+    
+    public Neurus() throws Exception{
+         
+    }
+    
+    public void iniciar() throws Exception{
+         int maxIterations = 1000;
+        NeuralNetwork neuralNet = new MultiLayerPerceptron(225,450,225,120,12);
+        ((LMS) neuralNet.getLearningRule()).setMaxError(0.08);//0-1
         ((LMS) neuralNet.getLearningRule()).setLearningRate(0.01);//0-1
-        ((LMS) neuralNet.getLearningRule()).setMaxIterations(maxIterations);//0-1
+       // ((LMS) neuralNet.getLearningRule()).setMaxIterations(maxIterations);//0-1
         
         
          // enable batch if using MomentumBackpropagation
-        if( neuralNet.getLearningRule() instanceof MomentumBackpropagation )
+       
+        
+        neuralNet.getLearningRule().addObserver(this);
+        
+        if( neuralNet.getLearningRule() instanceof MomentumBackpropagation ) 
             ((MomentumBackpropagation)neuralNet.getLearningRule()).setBatchMode(true);
         
-        TrainingSet trainingSet = new TrainingSet(2,1);
-
-        String imagePath = "D:\\Dropbox\\facu\\75.23 Inteligencia Artificial\\Tp Red Neuronal";
-        for (int i = 1; i <=3 ; i++) {
-            BufferedImage bi = ImageIO.read(new File(imagePath + "\\" + i + ".png"));
-            //trainingSet.addElement(new SupervisedTrainingElement(fractionRgbData(bi), new double[]{i/10}));
-            trainingSet.addElement(new SupervisedTrainingElement(new double[]{i/10,i/10}, new double[]{i/10}));
-            //trainingSet.addElement(new SupervisedTrainingElement(new double[]{i}, new double[]{i}));
-            
-        }
         
-        neuralNet.learnInSameThread(trainingSet);
+         String dir = System.getProperty("user.dir").concat("/src/neurus/");
+        agregarEntrenamiento(dir+"fuente1.png", neuralNet);
+        //agregarEntrenamiento(dir+"fuente2.png", neuralNet);
+//        agregarEntrenamiento(dir+"fuente3.png", neuralNet);
+        
         
       
         TrainingSet testSet = new TrainingSet();
-        BufferedImage bi = ImageIO.read(new File(imagePath + "\\test.png"));
-        a=1;
-        //testSet.addElement(new TrainingElement(fractionRgbData(bi)));
-        testSet.addElement(new TrainingElement(new double[]{3/10,3/10}));
-
+        
+        ExtractorCaracteres ec=new ExtractorCaracteres();
+        ArrayList<BufferedImage> ab=ec.recortar(new File(dir+"test.png"), new File("D:\\facu\\75.23 IA\\trunk\\Neurus\\src\\neurus\\"),15,15);
+        testSet.addElement(new TrainingElement(fractionRgbData(ab.get(0))));
+        
+//        do {            
+//            Thread.sleep(5*1000);
+//        } while (!neuralNet.getLearningRule().isStopped());
+//        
         for (TrainingElement testElement : testSet.trainingElements()) {
             neuralNet.setInput(testElement.getInput());
             neuralNet.calculate();
             double[] networkOutput = neuralNet.getOutput();
+            
             System.out.println(" Output: " + Arrays.toString(networkOutput));
+            System.out.println("Ganador:"+getWinner(networkOutput)+"!!!");
         }
 
+    }
+    public static void main(String[] args) throws IOException, Exception {
+        Neurus n=new Neurus();
+        n.iniciar();
+      
        
     }
 
-    static int a=0;
+    
+      public static int getWinner(double[] winers){ 
+              
+             double max = 0; 
+             int winner = 0; 
+              
+             for (int i = 0; i < winers.length; i++) { 
+                
+                  if (winers[i] > max){ 
+                        
+                       winner = i; 
+                       max = winers[i]; 
+                        
+                  } 
+                        
+                   
+                   
+          } 
+              
+             return winner; 
+        } 
+    
+    
+    
+    public static void agregarEntrenamiento(String url,NeuralNetwork net) throws Exception{
+        ExtractorCaracteres ec=new ExtractorCaracteres();
+        TrainingSet trainingSet = new TrainingSet(225,12);
+        ArrayList<BufferedImage> imgs=ec.recortar(new File(url), new File("D:\\facu\\75.23 IA\\trunk\\Neurus\\src\\neurus\\"), 15, 15);
+        
+        for (int i = 0; i <imgs.size(); i++) {
+            BufferedImage bufferedImage = imgs.get(i);
+            double[] rgbValues=fractionRgbData(bufferedImage);
+            double[] resultado=new double[12];
+               
+            Arrays.fill(resultado, 0);
+            resultado[i]=1;         
+            
+            trainingSet.addElement(new SupervisedTrainingElement(rgbValues, resultado));
+        }       
+        net.learnInSameThread(trainingSet);
+        System.out.println("Entrenado: "+url);
+//        net.save("redIntentoAmano");
+    }
+    
     public static double[] fractionRgbData(BufferedImage image) throws Exception {
-        FractionRgbData imgRgb = new FractionRgbData(ImageSampler.downSampleImage(new Dimension(15, 15), image));
+        //ImageSampler.downSampleImage(new Dimension(30,30),
+        FractionRgbData imgRgb = new FractionRgbData( image);
         double input[];
         
         
-            input = FractionRgbData.convertRgbInputToBinaryBlackAndWhite(imgRgb.getFlattenedRgbValues());
-           
-            System.out.println( Arrays.toString(input));
-            for (int i = 0; i < input.length; i++) {
-            input[i]=a;
-         
-            }
-            a++;
+           input = FractionRgbData.convertRgbInputToBinaryBlackAndWhite(imgRgb.getFlattenedRgbValues());
+//            System.out.println("Tamanio imput"+input.length);
+        //input = imgRgb.getFlattenedRgbValues();
+          // System.out.println("Tamanio imput"+input.length);
+//            System.out.println( Arrays.toString(input));
+           //Arrays.toString(input);
+            
         return input;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+         SupervisedLearning rule = (SupervisedLearning)o; 
+         if(rule!=null)
+          System.out.println( "Training, Network Epoch " + rule.getCurrentIteration() + ", Error:" + rule.getTotalNetworkError());
     }
 }
